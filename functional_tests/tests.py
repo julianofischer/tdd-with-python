40fe86d8e02a8f54eb145ee9e2fd8cc02e0a93c7
Juliano Fischer Naves <julianofischer@gmail.com>
2022-06-05 20:59:49 -0300

    refactoring voodoo waits

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,13 @@
+from ast import Assert
+from asyncio import wait_for
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 from django.test import LiveServerTestCase
 
+MAX_WAIT = 10
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
@@ -12,6 +16,19 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         #self.browser.quit()
         pass
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
@@ -27,17 +44,13 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+    
+
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('Finish the test!')
         
 
