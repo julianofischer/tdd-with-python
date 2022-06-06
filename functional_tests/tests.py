a2d3b15250871be450ea523ac22591fec5c89878
Juliano Fischer Naves <julianofischer@gmail.com>
2022-06-05 21:23:44 -0300

    functional tests now are checking for multiple users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -14,8 +14,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser = webdriver.Firefox()
     
     def tearDown(self):
-        #self.browser.quit()
-        pass
+        self.browser.quit()
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
@@ -52,7 +51,37 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('Finish the test!')
-        
+
+    
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(edith_list_url, francis_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Buy milk', page_text)
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
